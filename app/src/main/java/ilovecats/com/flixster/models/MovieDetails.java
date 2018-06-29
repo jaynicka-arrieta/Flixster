0c665ca93d06927db1bd9a0d0aacec4164d45130
Jaynicka Arrieta <kiira@fb.com>
2018-06-29 16:14:26 -0700

    just for good measure

---
+++ app/src/main/java/ilovecats/com/flixster/models/MovieDetails.java
@@ -0,0 +1,21 @@
+package ilovecats.com.flixster.models;
+
+import android.os.Bundle;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
+
+import com.loopj.android.http.AsyncHttpClient;
+
+import java.util.ArrayList;
+
+import ilovecats.com.flixster.MovieAdapter;
+import ilovecats.com.flixster.R;
+
+public class MovieDetails {
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.movie_details);
+
+
+    }
+}
